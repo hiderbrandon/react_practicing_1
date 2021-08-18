cc80a30092964e3aee0155a245dea076fa94eb0b
hiderbrandon <hiderbrandon@gmail.com>
2021-08-17 21:48:11 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Table from './table'
+
+class App extends React.Component {
+  render() {
+    return (
+    
+        <div className = 'container'>
+        <Table></Table>
+        </div>
+      
+    )
+  }
+}
+
+
+export default App;
